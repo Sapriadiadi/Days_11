a4894066de9e19bbf445a000e17ed230e869e17a
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-19 22:13:31 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package days_11;
+
+public class Main {
+    
+    public static void main(String[] args) {
+          { 
+     int a = 2, b = 2;
+     String jawaban;
+     
+     jawaban = a > b ? "iya" : "tidak";
+    System.out.println(jawaban);
+   }
+ }
+    }
+    
